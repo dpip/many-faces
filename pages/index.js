a73f441e473534577d5eddb536dda578f7036b46
dpip <dpip.nc@gmail.com>
2022-10-26 14:40:04 -0400

    fetches blogs from Home page // async call to getAllBlogs function in apijs

--- pages/index.js
+++ pages/index.js
@@ -2,18 +2,21 @@
 // import Nav from 'react-bootstrap/Nav';
 // import Navbar from 'react-bootstrap/Navbar';
 // import NavDropdown from 'react-bootstrap/NavDropdown';
-import { Row, Col, Media, Image, Card } from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 
 import PageLayout from 'components/PageLayout';
 import AuthorIntro from 'components/AuthorIntro';
 import CardItem from 'components/CardItem';
 import CardListItem from 'components/CardListItem';
 
-export default function Home() {
+import { getAllBlogs } from 'lib/api';
+
+const Home = ({ blogs }) => {
   return (
     <PageLayout>
       <AuthorIntro />
       <hr />
+      {JSON.stringify(blogs)}
       <Row className="mb-5">
         <Col md="10">
           <CardListItem />
@@ -24,4 +27,18 @@ export default function Home() {
       </Row>
     </PageLayout>
   );
+};
+
+export default Home;
+
+// The getStaticProps function is called during the build (build time)
+// Provides props to the page
+// Creates static page content
+export async function getStaticProps() {
+  const blogs = await getAllBlogs();
+  return {
+    props: {
+      blogs,
+    },
+  };
 }
