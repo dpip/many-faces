ebb87cf88b3141eec7badca6556e349b0388ef52
dpip <dpip.nc@gmail.com>
2022-10-27 15:17:00 -0400

    imports fonts into next head global componenet // minor styling changes

--- components/PageLayout.jsx
+++ components/PageLayout.jsx
@@ -1,14 +1,22 @@
 import { Container } from 'react-bootstrap'
+import Head from 'next/Head'
 import NavbarMain from './NavbarMain'
 import FooterMain from './FooterMain'
 
 const PageLayout = ({children, className}) => {
     return (
+        <>
+        <Head>
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+            <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700;900&display=swap" rel="stylesheet" />
+        </Head>
         <Container>
             <NavbarMain />
             <div className={`page-wrapper ${className}`}>{children}</div>
             <FooterMain />
         </Container>
+        </>
     )
 }
 
