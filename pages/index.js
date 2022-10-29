ad8cd328e149ea46fc00580d9dc6418efcbb343a
dpip <dpip.nc@gmail.com>
2022-10-29 17:49:24 -0400

    creates filtering component

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,45 @@
+import { useState } from 'react';
 import { Row, Col } from 'react-bootstrap';
 
 import PageLayout from 'components/PageLayout';
 import AuthorIntro from 'components/AuthorIntro';
 import CardItem from 'components/CardItem';
 import CardListItem from 'components/CardListItem';
+import FilteringMenu from 'components/FilteringMenu';
 
 import { getAllBlogs } from 'lib/api';
 
 const Home = ({ blogs }) => {
+  const [filter, setFilter] = useState({
+    view: { list: 1 },
+  });
   return (
     <PageLayout>
       <AuthorIntro />
+      <FilteringMenu onChange={() => {}} />
       <hr />
       <Row className="mb-5">
-        {/* <Col md="10">
-          <CardListItem />
-        </Col> */}
-        {blogs.map((blog) => (
-          <Col key={blog.slug} md="4">
-            <CardItem
-              title={blog.title}
-              subtitle={blog.subtitle}
-              date={blog.date}
-              image={blog.coverImage}
-              author={blog.author}
-              link={{
-                href: '/blogs/[slug]',
-                as: `/blogs/${blog.slug}`,
-              }}
-            />
-          </Col>
-        ))}
+        {blogs.map((blog) =>
+          filter.view.list ? (
+            <Col md="10">
+              <CardListItem />
+            </Col>
+          ) : (
+            <Col key={blog.slug} md="4">
+              <CardItem
+                title={blog.title}
+                subtitle={blog.subtitle}
+                date={blog.date}
+                image={blog.coverImage}
+                author={blog.author}
+                link={{
+                  href: '/blogs/[slug]',
+                  as: `/blogs/${blog.slug}`,
+                }}
+              />
+            </Col>
+          )
+        )}
       </Row>
     </PageLayout>
   );
