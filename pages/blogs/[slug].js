54abf81ce70cc1e5287953131d97c269a6620902
dpip <dpip.nc@gmail.com>
2022-11-02 19:16:42 -0400

    provides fallback for blog details component

--- pages/blogs/[slug].js
+++ pages/blogs/[slug].js
@@ -1,13 +1,28 @@
 import PageLayout from 'components/PageLayout';
 import BlogHeader from 'components/BlogHeader';
+import ErrorPage from 'next/error';
+import { getBlogBySlug, getAllBlogs } from 'lib/api';
 import { Row, Col } from 'react-bootstrap';
-import { getAllBlogs, getBlogBySlug } from 'lib/api';
 import { urlFor } from 'lib/api';
 import moment from 'moment';
+import { useRouter } from 'next/router';
 
 import BlogContent from 'components/BlogContent';
 
 const BlogDetail = ({ blog }) => {
+  const router = useRouter();
+
+  if (!router.isFallback && !blog?.slug) {
+    return <ErrorPage statusCode="404" />;
+  }
+
+  if (router.isFallback) {
+    console.log('Loading fallback page');
+    return (
+      <PageLayout className="blog-detail-page">Loading...</PageLayout>
+    );
+  }
+
   return (
     <PageLayout className="blog-detail-page">
       <Row>
@@ -15,9 +30,9 @@ const BlogDetail = ({ blog }) => {
           <BlogHeader
             title={blog.title}
             subtitle={blog.subtitle}
-            coverImage={urlFor(blog.coverImage).height(500).url()}
+            coverImage={urlFor(blog.coverImage).height(600).url()}
             author={blog.author}
-            date={moment(blog.date).format('LL')}
+            date={moment(blog.date).format('LLL')}
           />
           <hr />
           {blog.content && <BlogContent content={blog.content} />}
@@ -34,11 +49,13 @@ export async function getStaticProps({ params }) {
   };
 }
 
+// TODO: Introduce fallback
 export async function getStaticPaths() {
   const blogs = await getAllBlogs();
+  const paths = blogs?.map((b) => ({ params: { slug: b.slug } }));
   return {
-    paths: blogs?.map((b) => ({ params: { slug: b.slug } })),
-    fallback: false,
+    paths,
+    fallback: true,
   };
 }
 
