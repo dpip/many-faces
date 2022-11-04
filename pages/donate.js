17a8522da0da73a57ee358dd6b92b2dad7cd5426
dpip <dpip.nc@gmail.com>
2022-11-04 17:25:16 -0400

    adds stripe keys and comments to env

---
+++ pages/donate.js
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+
+import { Row, Button } from 'react-bootstrap';
+import PageLayout from 'components/PageLayout';
+
+export default function Donate() {
+  return (
+    <PageLayout>
+      <h1>Donate</h1>
+    </PageLayout>
+  );
+}
