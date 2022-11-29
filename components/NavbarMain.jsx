fbf87aae477cef8f383dae9f826a08971ba589b2
dpip <dpip.nc@gmail.com>
2022-11-28 19:23:48 -0500

    completes sticky nav on sroll for desktop

--- components/NavbarMain.jsx
+++ components/NavbarMain.jsx
@@ -1,117 +1,127 @@
-import {useEffect} from 'react'
+import { useState, useRef, useEffect } from 'react';
 import {
   Navbar,
   Nav,
-  Button
+  Button,
+  Container
 } from 'react-bootstrap';
 import Link from 'next/link'
 import ThemeToggle from 'components/ThemeToggle';
 import { useTheme } from 'providers/ThemeProvider';
 
 
-
 const NavbarMain = () => {
     const { theme, toggleTheme } = useTheme();
-    let timeout
-    let scroll = 0
-      useEffect(() => {
-        window.onscroll = () => {
-          if (timeout) {
-            clearTimeout(timeout)
-          }
+    const [sticky, setSticky] = useState({ isSticky: false, offset: 0 });
+    const headerRef = useRef(null);
 
-          timeout = setTimeout(() => {
-            if (scroll >= window.scrollY && window.scrollY > 10) {
-              document.getElementById('navbar').classList.add('sticky')
-              alert('if test')
-            } else {
-              document.getElementById('navbar').classList.remove('sticky')
-              alert('else test')
-            }
+    // handle scroll event
+    const handleScroll = (elTopOffset, elHeight) => {
+      if (window.pageYOffset > (elTopOffset + elHeight)) {
+        setSticky({ isSticky: true, offset: elHeight });
+      } else {
+        setSticky({ isSticky: false, offset: 0 });      }
+    };
 
-            scroll = window.scrollY
-          }, 10)
-        }
-      }, [])
+    // add/remove scroll event listener
+    useEffect(() => {
+      var header = headerRef.current.getBoundingClientRect();
+      const handleScrollEvent = () => {
+        handleScroll(header.top, header.height)
+      }
+
+      window.addEventListener('scroll', handleScrollEvent);
+
+      return () => {
+        window.removeEventListener('scroll', handleScrollEvent);
+      };
+    }, []);
+
     return (
-        <Navbar
-        id={'navbar'}
-        className="fj-navbar fj-nav-base"
-        bg="transparent"
-        expand="lg"
-      >
-        <Navbar.Brand className="fj-navbar-brand">
-          <Link href="/">
-            <a>Many Faces</a>
-          </Link>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav" className="d-flex justify-content-center">
-          <Nav className="text-capitalize">
-              <Nav.Link
-              className="fj-navbar-item fj-navbar-link"
-              as={() =>
-                <Link href='/apply'>
-                  <a className="fj-navbar-item fj-navbar-link text-capitalize">Apply</a>
-                </Link>
-              }
-              />
-              <Nav.Link
-              className="fj-navbar-item fj-navbar-link"
-              as={() =>
-                <Link href='/donate'>
-                  <a className="fj-navbar-item fj-navbar-link text-capitalize">Donate</a>
-                </Link>
-              }
-              />
-              <Nav.Link
-              className="fj-navbar-item fj-navbar-link"
-              as={() =>
-                <Link href='/about'>
-                  <a className="fj-navbar-item fj-navbar-link text-capitalize">About</a>
-                </Link>
-              }
-              />
-              <Nav.Link
-              className="fj-navbar-item fj-navbar-link"
-              as={() =>
-                <Link href='/join'>
-                  <a className="fj-navbar-item fj-navbar-link text-capitalize">Join</a>
-                </Link>
-              }
-              />
-              <Nav.Link
-              className="fj-navbar-item fj-navbar-link"
-              as={() =>
-                <Link href='/blog'>
-                  <a className="fj-navbar-item fj-navbar-link text-capitalize">Blog</a>
-                </Link>
-              }
-              />
-              <Nav.Link
-              className="fj-navbar-item fj-navbar-link"
-              as={() =>
-                <Link href='/contact'>
-                  <a className="fj-navbar-item fj-navbar-link text-capitalize">Contact</a>
-                </Link>
-              }
-              />
-          </Nav>
-        </Navbar.Collapse>
-        <div style={{ textAlign: 'center' }}>
-          <ThemeToggle onChange={toggleTheme} />
-          {/* <button className="btn btn-success" onClick={toggleTheme}>
-            {theme.type}
-          </button> */}
-        {/* <Button
-          // onClick={}
-          size="lg"
-          variant="outline-primary"
-        >
-          Donate
-        </Button> */}
-      </div>
-      </Navbar>
+      <Container style={{height: '100px'}}>
+        <div className={`${sticky.isSticky ? 'sticky' : ''} d-flex justify-content-center`} style={{padding: sticky.isSticky ? '0px 100px 0px 100px' : '0px 15px 0px 15px', boxShadow: sticky.isSticky ? '0px 1px 10px #999' : '', borderRadius: '2px'}}>
+          <Navbar
+            id={'navbar'}
+            className={`fj-navbar fj-nav-base`}
+            bg="transparent"
+            expand="lg"
+            ref={headerRef}
+            style={{maxWidth: '1140px'}}
+          >
+            <Navbar.Brand className="fj-navbar-brand">
+              <Link href="/">
+                <a>Many Faces</a>
+              </Link>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav" className="d-flex justify-content-center">
+              <Nav className="text-capitalize">
+                  <Nav.Link
+                  className="fj-navbar-item fj-navbar-link"
+                  as={() =>
+                    <Link href='/apply'>
+                      <a className="fj-navbar-item fj-navbar-link text-capitalize">Apply</a>
+                    </Link>
+                  }
+                  />
+                  <Nav.Link
+                  className="fj-navbar-item fj-navbar-link"
+                  as={() =>
+                    <Link href='/donate'>
+                      <a className="fj-navbar-item fj-navbar-link text-capitalize">Donate</a>
+                    </Link>
+                  }
+                  />
+                  <Nav.Link
+                  className="fj-navbar-item fj-navbar-link"
+                  as={() =>
+                    <Link href='/about'>
+                      <a className="fj-navbar-item fj-navbar-link text-capitalize">About</a>
+                    </Link>
+                  }
+                  />
+                  <Nav.Link
+                  className="fj-navbar-item fj-navbar-link"
+                  as={() =>
+                    <Link href='/join'>
+                      <a className="fj-navbar-item fj-navbar-link text-capitalize">Join</a>
+                    </Link>
+                  }
+                  />
+                  <Nav.Link
+                  className="fj-navbar-item fj-navbar-link"
+                  as={() =>
+                    <Link href='/blog'>
+                      <a className="fj-navbar-item fj-navbar-link text-capitalize">Blog</a>
+                    </Link>
+                  }
+                  />
+                  <Nav.Link
+                  className="fj-navbar-item fj-navbar-link"
+                  as={() =>
+                    <Link href='/contact'>
+                      <a className="fj-navbar-item fj-navbar-link text-capitalize">Contact</a>
+                    </Link>
+                  }
+                  />
+              </Nav>
+          </Navbar.Collapse>
+            <div style={{ textAlign: 'center' }}>
+                <ThemeToggle onChange={toggleTheme} />
+                {/* <button className="btn btn-success" onClick={toggleTheme}>
+                  {theme.type}
+                </button> */}
+              {/* <Button
+                // onClick={}
+                size="lg"
+                variant="outline-primary"
+              >
+                Donate
+              </Button> */}
+            </div>
+          </Navbar>
+        </div>
+      </Container>
     )
 }
 
--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-svg-core/styles.css';
 import 'react-toggle/style.css';
 import 'styles/index.scss';
+import 'styles/navbar.scss';
 import { AnimatePresence } from 'framer-motion';
 
 import ThemeProvider from 'providers/ThemeProvider';
