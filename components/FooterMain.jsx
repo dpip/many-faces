75a1dface3aad035600d2cefc7e7548eaf04c6bc
dpip <dpip.nc@gmail.com>
2022-12-19 12:26:48 -0500

    updates to footer

--- components/FooterMain.jsx
+++ components/FooterMain.jsx
@@ -1,14 +1,16 @@
-
+import Link from 'next/link'
 
 const FooterMain = () => {
     return (
-        <footer className="page-footer">
-            <div>
-            <a href="#">twitter</a>
-            {' | '}
-            <a href="#">facebook</a>
-            {' | '}
-            <a href="#">email</a>
+        <footer className="page-footer d-flex justify-content-between pb-4 pt-3">
+            <div>Logo</div>
+            <div className={'text-sm'}>
+                <small>&copy; 2021 The Many Faces Initiative</small>
+            </div>
+            <div>   
+                <Link href='/contact'>
+                    <a className="fj-navbar-item fj-navbar-link text-capitalize"><small>Contact</small></a>
+                </Link>
             </div>
         </footer>
     )
