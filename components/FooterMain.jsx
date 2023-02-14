4b61c25b3dfa49c6f1ccb7ee97713ab6090e703a
dpip <dpip.nc@gmail.com>
2023-02-14 14:27:08 -0500

    adds footer updates from main branch with no conflicts

--- components/FooterMain.jsx
+++ components/FooterMain.jsx
@@ -1,15 +1,22 @@
 import Link from 'next/link'
+import Image from 'next/image'
 
 const FooterMain = () => {
     return (
         <footer className="page-footer d-flex justify-content-between pb-4 pt-3">
-            <div>Logo</div>
-            <div className={'text-sm'}>
-                <small>&copy; 2021 The Many Faces Initiative</small>
+            <div>
+                <Link href="/">
+                    <a>
+                        <Image src="/ManyFaces_Initiative_Logo_ICON.png" alt="Many Faces" width="42" height="42" />
+                    </a>
+                </Link>
+              </div>
+            <div className={'text-sm d-flex justisy-content-center align-items-center'}>
+                <small>&copy; 2020 The Many Faces Initiative</small>
             </div>
-            <div>   
+            <div className={'text-sm'}>   
                 <Link href='/contact'>
-                    <a className="fj-navbar-item fj-navbar-link text-capitalize"><small>Contact</small></a>
+                    <a className="fj-navbar-item fj-navbar-link text-capitalize h-100 w-100 d-flex justisy-content-center align-items-center"><small>Contact</small></a>
                 </Link>
             </div>
         </footer>
