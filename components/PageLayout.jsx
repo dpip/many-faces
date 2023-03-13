cb68147ee5c789dc270200effc04425eba13639c
dpip <dpip.nc@gmail.com>
2023-03-13 18:28:34 -0400

    adds document component to resolvestyle sheet warnings

--- components/PageLayout.jsx
+++ components/PageLayout.jsx
@@ -17,11 +17,6 @@ const PageLayout = ({children, className}) => {
     const { theme, toggleTheme } = useTheme();
     return (
         <div className={theme.type}>
-        <Head>
-            <link rel="preconnect" href="https://fonts.googleapis.com" />
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-            <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700;900&display=swap" rel="stylesheet" />
-        </Head>
         <Container>
             <NavbarMain
                 theme={theme}
