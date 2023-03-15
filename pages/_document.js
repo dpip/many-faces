fabcb22261d5e1a4688f67110f29e709261445ba
dpip <dpip.nc@gmail.com>
2023-03-15 17:53:13 -0400

    pausing on og meta tags to focus on page transitions

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,20 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <title>The Many Faces Initiative</title>
+        <meta property="og:url" content="your url" />
+        <meta property="og:type" content="website" />
+        <meta property="fb:app_id" content="your fb app id" />
+        <meta
+          property="og:title"
+          content="Social Media Preview Working?"
+        />
+        <meta name="twitter:card" content="summary" />
+        <meta
+          property="og:description"
+          content="Hurray!! Yes Social Media Preview is Working"
+        />
+        <meta property="og:image" content={'url of image'} />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
